99c6328f30e80effba115b32e5ed335a04f27692
reneRomero <rene.romero@badak.mx>
2020-01-16 16:10:23 -0600

    proyecto inicial

---
+++ src/main/java/com/mx/jr/cendi/bienesconsumibles/BienesConsumiblesApplication.java
@@ -0,0 +1,13 @@
+package com.mx.jr.cendi.bienesconsumibles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BienesConsumiblesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BienesConsumiblesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mx/jr/cendi/bienesconsumibles/BienesConsumiblesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.jr.cendi.bienesconsumibles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BienesConsumiblesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
